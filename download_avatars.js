df27236e553e9b3dfcaaffa1d94a1ded78cc7351
jerry1646 <jerrysong131@gmail.com>
2018-10-01 23:22:36 +0000

    Add authorization

---
+++ download_avatars.js
@@ -0,0 +1,34 @@
+var request = require('request');
+var token = require('./secrets');
+
+console.log("Welcome to the GitHub Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+
+  var options = {
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token
+    },
+    url:`https://api.github.com/repos/${repoOwner}/${repoName}/contributors`
+  };
+
+  request.get(options, function(err,res,body){
+    cb(err,body);
+
+  })
+        // .on("error", function(err){
+        //   throw err;
+        // })
+        // .on("response", function(response){
+        //   console.log(`Response Code: ${response.statusCode}`);
+        //   console.log(response);
+        // });
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
