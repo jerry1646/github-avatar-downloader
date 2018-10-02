ed485b2bbb493a9b916b98cb5309a481390469c4
jerry1646 <jerrysong131@gmail.com>
2018-10-02 00:35:20 +0000

    add error handler for missing .env and for missing ./avatars directory

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,17 @@
-require('dotenv').config();
 var request = require('request');
 var fs = require('fs');
 var args = process.argv.slice(2);
 
+if(!fs.existsSync("./.env")){
+  console.log("Environment configuration missing. Please check if .env file is properly setup in your application root folder");
+  return;
+}
+
+require('dotenv').config();
+
 
 console.log("Welcome to the GitHub Avatar Downloader!");
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
@@ -23,14 +30,17 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 
 function downloadImageByURL(object) {
-  for (const keys in object){
-    var user  = object[keys];
-    console.log(user.avatar_url)
-    request.get(user.avatar_url)
+  if(fs.existsSync("./avatars")){
+    for (const keys in object){
+      var user  = object[keys];
+      console.log("Downloading avatar image from: "+user.avatar_url)
+      request.get(user.avatar_url)
       .on("error", function(err){
-        throw err;
-      })
+        throw err;})
       .pipe(fs.createWriteStream("./avatars/" + user.login + ".jpg"));
+    }
+  } else{
+    console.log("Indicated directory doesn't exist. Please create ./avatars folder and try again!")
   }
 }
 
